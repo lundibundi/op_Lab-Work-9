0acbba8dedac60d9d1d17359b127a200ebd779f0
lundibundi <shishugi@gmail.com>
2016-05-12 14:43:22 +0300

    code clean up

--- Lab_9/Lab_9/Lab_9.cpp
+++ Lab_9/Lab_9/Lab_9.cpp
@@ -3,26 +3,8 @@
 #include <conio.h>
 #include "stdafx.h"
 
-namespace x{
-	int n = 10;
-}
-
-namespace y{
-	int n = 20;
-}
 int main()
 {
-	bool way = true;
-	int n = 100;
-	{
-		if (way) using namespace x;
-		else using namespace y;
-		printf("%d\n", n);
-	
-	}
-
-//	_getch();
-	printf("%d", n);
 	_getch();
     return 0;
 }
